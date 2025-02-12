532fe8adaf09df59fac4cf01ff7f2420d37ed07a
bigCoDult <qqqqmmss11@gmail.com>
2025-02-12 13:10:46 +0900

    [add] ft_memset 추가

--- src/ft.c
+++ src/ft.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yutsong <yutsong@student.42gyeongsan.kr    +#+  +:+       +#+        */
+/*   By: sanbaek <sanbaek@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 03:48:14 by yutsong           #+#    #+#             */
-/*   Updated: 2025/02/05 13:24:33 by yutsong          ###   ########.fr       */
+/*   Updated: 2025/02/12 13:10:26 by sanbaek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,4 +61,17 @@ char *ft_strdup(const char *str)
         return (NULL);
     memcpy(dup, str, len);
     return (dup);
+}
+
+// main에서 shell 변수 이니셜라이즈를 하기위함
+void	*ft_memset(void *dest, int c, size_t count)
+{
+	unsigned char	*dest_tmp;
+	unsigned char	unsigned_char_c;
+
+	dest_tmp = (unsigned char *)dest;
+	unsigned_char_c = (unsigned char)c;
+	while (count--)
+		*dest_tmp++ = unsigned_char_c;
+	return (dest);
 }
