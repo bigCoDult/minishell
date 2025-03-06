b255b1c71a62ae114e1f4845b7ec29461bc9eca8
bigCoDult <qqqqmmss11@gmail.com>
2025-03-06 12:00:39 +0900

    [fix] 파이프 종료시 free_exit()

--- src/execute_pipe.c
+++ src/execute_pipe.c
@@ -202,5 +202,6 @@ int	execute_pipe(t_shell *shell, t_ast_node *node)
 	close(pipefd[1]);
 	waitpid(pid1, &status1, 0);
 	waitpid(pid2, &status2, 0);
+	free_exit(shell, 0);
 	return (WEXITSTATUS(status2));
 }
