de406a460e0fa7f1c9cb0e87ebcd94ea65724fa0
macstudio <tm1361@naver.com>
2025-03-13 09:03:34 +0000

    [fix] norm#8

--- src/redir_utils3.c
+++ src/redir_utils3.c
@@ -6,36 +6,31 @@
 /*   By: yutsong <yutsong@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 08:46:17 by yutsong           #+#    #+#             */
-/*   Updated: 2025/03/13 08:50:45 by yutsong          ###   ########.fr       */
+/*   Updated: 2025/03/13 09:02:52 by yutsong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	handle_input_redirections(t_shell *shell, t_redirection *redirs)
+static int	process_single_input(t_redirection *current, int *last_fd)
 {
-	t_redirection	*current;
-	int				last_fd;
-
-	last_fd = -1;
-	current = redirs;
-	while (current)
+	if (current->type == REDIR_IN)
 	{
-		if (current->type == REDIR_IN)
+		if (*last_fd != -1)
+			close(*last_fd);
+		*last_fd = open(current->filename, O_RDONLY);
+		if (*last_fd == -1)
 		{
-			if (last_fd != -1)
-				close(last_fd);
-			last_fd = open(current->filename, O_RDONLY);
-			if (last_fd == -1)
-			{
-				print_error("minishell: %s: No such file or directory\n",
-					current->filename);
-				restore_io(shell);
-				return (1);
-			}
+			print_error("minishell: %s: No such file or directory\n",
+				current->filename);
+			return (1);
 		}
-		current = current->next;
 	}
+	return (0);
+}
+
+static int	setup_final_input(int last_fd, t_shell *shell)
+{
 	if (last_fd != -1)
 	{
 		if (dup2(last_fd, STDIN_FILENO) == -1)
@@ -47,4 +42,23 @@ int	handle_input_redirections(t_shell *shell, t_redirection *redirs)
 		close(last_fd);
 	}
 	return (0);
+}
+
+int	handle_input_redirections(t_shell *shell, t_redirection *redirs)
+{
+	t_redirection	*current;
+	int				last_fd;
+
+	last_fd = -1;
+	current = redirs;
+	while (current)
+	{
+		if (process_single_input(current, &last_fd))
+		{
+			restore_io(shell);
+			return (1);
+		}
+		current = current->next;
+	}
+	return (setup_final_input(last_fd, shell));
 }
