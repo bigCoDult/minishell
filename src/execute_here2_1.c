840881a99975e3bd1fe82a971391418549c7553a
macstudio <tm1361@naver.com>
2025-03-13 07:21:08 +0000

    [fix] norm#1

--- src/execute_here2_1.c
+++ src/execute_here2_1.c
@@ -6,7 +6,7 @@
 /*   By: yutsong <yutsong@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 03:00:36 by yutsong           #+#    #+#             */
-/*   Updated: 2025/03/13 04:48:08 by yutsong          ###   ########.fr       */
+/*   Updated: 2025/03/13 07:07:46 by yutsong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,63 +33,4 @@ void	check_delimiter_match(char *line, char *delimiter,
 		close(fd);
 		free_exit(shell, 0);
 	}
-}
-
-void	process_heredoc_lines(t_shell *shell, char *delimiter, int fd)
-{
-	char	*line;
-	int		original_stdout;
-	int		original_stderr;
-	char	buffer[4096];
-	int		len;
-
-	(void)shell;
-	check_heredoc_signal(fd);
-	original_stdout = dup(STDOUT_FILENO);
-	original_stderr = dup(STDERR_FILENO);
-	if (original_stdout == -1 || original_stderr == -1)
-	{
-		if (original_stdout != -1)
-			close(original_stdout);
-		if (original_stderr != -1)
-			close(original_stderr);
-		handle_null_line(shell, fd);
-	}
-	while (1)
-	{
-		check_heredoc_signal(fd);
-		write(STDERR_FILENO, "> ", 2);
-		line = NULL;
-		len = 0;
-		while ((len = read(STDIN_FILENO, buffer, sizeof(buffer) - 1)) > 0)
-		{
-			buffer[len] = '\0';
-			char *newline = ft_strchr(buffer, '\n');
-			if (newline)
-			{
-				*newline = '\0';
-				line = ft_strdup(buffer);
-				break ;
-			}
-		}
-		if (!line)
-		{
-			close(original_stdout);
-			close(original_stderr);
-			handle_null_line(shell, fd);
-		}
-		if (ft_strcmp(line, delimiter) == 0)
-		{
-			free(line);
-			close(original_stdout);
-			close(original_stderr);
-			close(fd);
-			free_exit(shell, 0);
-		}
-		write(fd, line, strlen(line));
-		write(fd, "\n", 1);	
-		free(line);
-	}
-	close(original_stdout);
-	close(original_stderr);
 }
---
+++ src/execute_here6.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute_here6.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yutsong <yutsong@student.42gyeongsan.kr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/13 07:07:48 by yutsong           #+#    #+#             */
+/*   Updated: 2025/03/13 07:19:14 by yutsong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static void	setup_heredoc_io(t_shell *shell, int io_fds[3])
+{
+	io_fds[0] = dup(STDOUT_FILENO);
+	io_fds[1] = dup(STDERR_FILENO);
+	if (io_fds[0] == -1 || io_fds[1] == -1)
+	{
+		if (io_fds[0] != -1)
+			close(io_fds[0]);
+		if (io_fds[1] != -1)
+			close(io_fds[1]);
+		handle_null_line(shell, io_fds[2]);
+	}
+}
+
+static char	*read_heredoc_line(void)
+{
+	char	buffer[4096];
+	char	*line;
+	int		len;
+	char	*newline;
+
+	write(STDERR_FILENO, "> ", 2);
+	line = NULL;
+	len = read(STDIN_FILENO, buffer, sizeof(buffer) - 1);
+	while (len > 0)
+	{
+		buffer[len] = '\0';
+		newline = ft_strchr(buffer, '\n');
+		if (newline)
+		{
+			*newline = '\0';
+			line = ft_strdup(buffer);
+			break ;
+		}
+	}
+	return (line);
+}
+
+static int	process_line(t_shell *shell, char *line,
+	char *delimiter, int io_fds[3])
+{
+	if (!line)
+	{
+		close(io_fds[0]);
+		close(io_fds[1]);
+		handle_null_line(shell, io_fds[2]);
+		return (0);
+	}
+	if (ft_strcmp(line, delimiter) == 0)
+	{
+		free(line);
+		close(io_fds[0]);
+		close(io_fds[1]);
+		close(io_fds[2]);
+		free_exit(shell, 0);
+		return (1);
+	}
+	write(io_fds[2], line, strlen(line));
+	write(io_fds[2], "\n", 1);
+	free(line);
+	return (0);
+}
+
+static void	heredoc_main_loop(t_shell *shell, char *delimiter, int io_fds[3])
+{
+	char	*line;
+
+	while (1)
+	{
+		check_heredoc_signal(io_fds[2]);
+		line = read_heredoc_line();
+		if (process_line(shell, line, delimiter, io_fds))
+			break ;
+	}
+}
+
+void	process_heredoc_lines(t_shell *shell, char *delimiter, int fd)
+{
+	int	io_fds[3];
+
+	io_fds[2] = fd;
+	check_heredoc_signal(fd);
+	setup_heredoc_io(shell, io_fds);
+	heredoc_main_loop(shell, delimiter, io_fds);
+	close(io_fds[0]);
+	close(io_fds[1]);
+}
